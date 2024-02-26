87db56f46aed571eeb5b361e5c8efaed7054c920
samarth <samarthkulkarni@icloud.com>
2024-02-26 22:14:35 +0530

    create directory works fully

--- create-directory.py
+++ create-directory.py
@@ -1,10 +1,20 @@
 import os
+import sys
 
 def create_structure(base_directory):
-    subdirectories = ["playlists", "songs", "hash-table"]
+    subdirectories = ["playlists", "hash-table"]
 
     if not os.path.exists(base_directory):
         os.makedirs(base_directory)
 
     for directory in subdirectories:
         os.makedirs(os.path.join(base_directory, directory), exist_ok=True)
+
+    file_path = base_directory + r"\hash-table\hash-table.txt"
+
+    if not os.path.exists(file_path):
+        with open(file_path, 'w') as file:
+            file.write("")
+
+if len(sys.argv) > 1:
+    create_structure(sys.argv[1])
