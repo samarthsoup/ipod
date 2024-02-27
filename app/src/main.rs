3ff89c2b1b91eaa074b95aea94290a8f764a5df4
samarth <samarthkulkarni@icloud.com>
2024-02-27 22:27:24 +0530

    load playlist

--- app/src/main.rs
+++ app/src/main.rs
@@ -8,7 +8,7 @@ fn get_base_directory() -> Result<String, Box<dyn Error>> {
     env::var("BASE_DIR")
 }
 
-fn load_playlist(playlist_name: String) -> Result<(), Box<dyn Error>> {
+fn load_playlist(playlist_name: String, queue: &mut Vec<String>) -> Result<(), Box<dyn Error>> {
     let base_directory = get_base_directory()?;
 
     let playlist_file_name = base_directory + r"\playlists\" + playlist_name.as_str(); 
@@ -17,8 +17,11 @@ fn load_playlist(playlist_name: String) -> Result<(), Box<dyn Error>> {
     let mut contents = String::new();
     playlist_file.read_to_string(&mut contents);
 
-    // parse playlist file function
-    // load it into queue
+    let reader = BufReader::new(playlist_file);
+
+    for line in reader.lines() {
+        queue.push(line)?;
+    }
 
     Ok(())
 }
