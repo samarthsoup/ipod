88afb1d3c37b9e609e0b07a8a3a4c6c364764135
samarth <samarthkulkarni@icloud.com>
2024-02-27 23:02:30 +0530

    code to song title

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, fs::File, io::Read, process};
+use std::{collections::HashMap, error::Error, fs::File, io::Read, process};
 use dotenv::dotenv;
 use std::env;
 
@@ -8,14 +8,18 @@ fn get_base_directory() -> Result<String, Box<dyn Error>> {
     env::var("BASE_DIR")
 }
 
+fn get_songs_directory() -> Result<String, Box<dyn Error>> {
+    dotenv().ok();
+
+    env::var("SONGS_DIR")
+}
+
 fn load_playlist(playlist_name: String, queue: &mut Vec<String>) -> Result<(), Box<dyn Error>> {
     let base_directory = get_base_directory()?;
 
     let playlist_file_name = base_directory + r"\playlists\" + playlist_name.as_str(); 
 
     let mut playlist_file = File::open(playlist_file_name)?;
-    let mut contents = String::new();
-    playlist_file.read_to_string(&mut contents);
 
     let reader = BufReader::new(playlist_file);
 
@@ -24,6 +28,29 @@ fn load_playlist(playlist_name: String, queue: &mut Vec<String>) -> Result<(), B
     }
 
     Ok(())
+}
+
+fn code_to_song_title(code: String) -> Result<String, Box<dyn Error>> {
+    let base_directory = get_base_directory()?;
+
+    let hash_table_name = base_directory + r"\hash-table\hash-table.txt"; 
+
+    let mut hash_table = File::open(hash_table_name)?;
+
+    codes = HashMap::new();
+
+    let reader = BufReader::new(hash_table);
+
+    for line in reader.lines() {
+        if let Some((key,value)) = line.split_once(":"){
+            if value == code {
+                return key;
+            } else {
+                return Box::new("delimiter not found");
+            }
+        } 
+            
+    }
 }
 
 fn main() {
