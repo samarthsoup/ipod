ddd3e1450981e92a03ce317ff38bc77e4739f666
samarth <samarthkulkarni@icloud.com>
2024-03-02 16:48:20 +0530

    playlist functions

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -1,8 +1,9 @@
-use std::{error::Error, fs::File, sync::mpsc::Sender};
+use std::{error::Error, fs::{self, File, OpenOptions}, sync::mpsc::Sender};
 use rodio::{Decoder, OutputStream, Sink, OutputStreamHandle};
 use std::{io::{self, BufReader, Write}, thread, sync::mpsc};
 use std::time::Duration;
 use std::io::BufRead;
+use std::path::Path;
 
 pub enum InterruptMessage {
     Play(String),
@@ -12,11 +13,17 @@ pub enum InterruptMessage {
     Resume,
     Next,
     Previous,
-    LoadPlaylist(String)
+    LoadPlaylist(String),
+    CreatePlaylist(String),
+    DeletePlaylist(String),
+    AddToPlaylist(String),
+    RemoveFromPlaylist(String),
+    QueueToPlaylist(String),
+    DisplayQueue,
 }
 
 fn get_base_directory() -> Result<String, Box<dyn Error>> {
-    let env_name = "env.txt";
+    let env_name = ".env";
     let env = File::open(env_name)?;
 
     let reader = BufReader::new(env);
@@ -33,7 +40,7 @@ fn get_base_directory() -> Result<String, Box<dyn Error>> {
 }
 
 fn get_songs_directory() -> Result<String, Box<dyn Error>> {
-    let env_name = "env.txt";
+    let env_name = ".env";
     let env = File::open(env_name)?;
 
     let reader = BufReader::new(env);
@@ -51,11 +58,8 @@ fn get_songs_directory() -> Result<String, Box<dyn Error>> {
 
 fn load_playlist(playlist_name: String, queue: &mut Vec<String>) -> Result<(), Box<dyn Error>> {
     let base_directory = get_base_directory()?;
-
     let playlist_file_name = base_directory + r"\playlists\" + playlist_name.as_str(); 
-
     let playlist_file = File::open(playlist_file_name)?;
-
     let reader = BufReader::new(playlist_file);
 
     for line in reader.lines() {
@@ -65,13 +69,100 @@ fn load_playlist(playlist_name: String, queue: &mut Vec<String>) -> Result<(), B
     Ok(())
 }
 
+fn create_playlist(playlist_name: String) -> Result<(), Box<dyn Error>> {
+    let base_directory = get_base_directory()?;
+    let playlist_file_name = base_directory + r"\playlists\" + playlist_name.as_str();
+    File::create(playlist_file_name)?;
+    Ok(())
+}
+
+fn delete_playlist(playlist_name: String) -> Result<(), Box<dyn Error>> {
+    let base_directory = get_base_directory()?;
+    let playlist_file_name = base_directory + r"\playlists\" + playlist_name.as_str();
+    fs::remove_file(playlist_file_name)?;
+    Ok(())
+}
+
+fn add_to_playlist(playlist_name: String, code: String) -> Result<(), Box<dyn Error>> {
+    let code_existence = match does_song_code_exist(&code) {
+        Ok(true) => true,
+        Ok(false) => false,
+        Err(e) => return Err(e)
+    };
+    if !code_existence {return Err("code DNE".into());}
+    let base_directory = get_base_directory()?;
+    let playlist_file_name = base_directory + r"\playlists\" + playlist_name.as_str();
+
+    let mut playlist_file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(playlist_file_name)?;
+
+    let code_with_newline = format!("\n{}", code);
+
+    playlist_file.write_all(code_with_newline.as_bytes())?;
+
+    Ok(())
+}
+
+fn remove_nth_line<P: AsRef<Path> + std::convert::AsRef<std::path::Path>>(path: P, n: usize) -> std::io::Result<()> {
+    let file = File::open(&path)?;
+    let buf_reader = BufReader::new(file);
+    let lines: Vec<String> = buf_reader.lines().collect::<Result<_, _>>()?;
+
+    if lines.len() > n {
+        let mut file = File::create(&path)?;
+        for (i, line) in lines.into_iter().enumerate() {
+            if i != n {
+                writeln!(file, "{}", line)?;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn remove_from_playlist(playlist_name: String, n: usize) -> Result<(), Box<dyn Error>> {
+    let base_directory = get_base_directory()?;
+    let playlist_file_name = base_directory + r"\playlists\" + playlist_name.as_str();
+
+    remove_nth_line(playlist_file_name, n)?;
+    Ok(())
+}
+
+fn queue_to_playlist(playlist_name: String, queue: &Vec<String>) -> Result<(), Box<dyn Error>> {
+    let base_directory = get_base_directory()?;
+    let playlist_file_name = base_directory + r"\playlists\" + playlist_name.as_str();
+
+    let mut file = File::create(playlist_file_name)?;
+    for i in queue {
+        writeln!(file, "{i}")?;
+    }
+
+    Ok(())
+}
+
+fn does_song_code_exist(code: &str) -> Result<bool, Box<dyn Error>> {
+    let base_directory = get_base_directory()?;
+    let hash_table_name = base_directory + r"\hash-table\hash-table.txt"; 
+    let hash_table = File::open(hash_table_name)?;
+    let reader = BufReader::new(hash_table);
+
+    for line in reader.lines() {
+        if let Some((_,value)) = line?.split_once(":"){
+            if value.trim() == code {
+                return Ok(true);
+            }
+        }      
+    };
+
+    return Ok(false);
+}
+
 fn code_to_song_title(code: String) -> Result<String, Box<dyn Error>> {
     let base_directory = get_base_directory()?;
-
     let hash_table_name = base_directory + r"\hash-table\hash-table.txt"; 
-
     let hash_table = File::open(hash_table_name)?;
-
     let reader = BufReader::new(hash_table);
 
     for line in reader.lines() {
@@ -87,26 +178,10 @@ fn code_to_song_title(code: String) -> Result<String, Box<dyn Error>> {
 
 fn play_track(sink: &mut Option<Sink>, track_name: String, stream_handle: &OutputStreamHandle) -> Result<(), Box<dyn std::error::Error>> {
     let songs_directory = get_songs_directory()?;
-
     let track_path = songs_directory + track_name.as_str();
-    let file = match File::open(track_path){
-        Ok(f) => f,
-        Err(e) => {
-            eprintln!("io error: {}", e);
-            print!(": ");
-            io::stdout().flush().unwrap();
-            return Err(Box::new(e));
-        }
-    };
-    let source = match Decoder::new(BufReader::new(file)){
-        Ok(s) => s,
-        Err(e) => {
-            eprintln!("decoder error: {}", e);
-            print!(": ");
-            io::stdout().flush().unwrap();
-            return Err(Box::new(e)); 
-        }
-    };
+    let file = File::open(track_path)?;
+
+    let source = Decoder::new(BufReader::new(file))?;
 
     if let Some(ref mut s) = sink {
         s.stop();
@@ -170,6 +245,7 @@ pub fn play_mp3(rx: mpsc::Receiver<InterruptMessage>) -> Result<(), Box<dyn std:
             InterruptMessage::Next => {
                 if !queue.is_empty() {
                     current_track = (current_track + 1) % queue.len();
+                    println!("{current_track}");
                     let file_path = code_to_song_title(queue[current_track].clone())?;
 
                     if let Some(ref s) = sink {
@@ -202,7 +278,37 @@ pub fn play_mp3(rx: mpsc::Receiver<InterruptMessage>) -> Result<(), Box<dyn std:
             },
             InterruptMessage::LoadPlaylist(playlist_name) => {
                 load_playlist(playlist_name, &mut queue)?;
-            }
+            }, 
+            InterruptMessage::CreatePlaylist(playlist_name) => {
+                create_playlist(playlist_name)?;
+            },
+            InterruptMessage::DeletePlaylist(playlist_name) => {
+                delete_playlist(playlist_name)?;
+            },
+            InterruptMessage::AddToPlaylist(data) => {
+                let data_vec:Vec<&str> = data.split_whitespace().collect();
+
+                if data_vec.len() != 2 {
+                    continue;
+                }
+
+                add_to_playlist(data_vec[0].to_string(), data_vec[1].to_string())?;
+
+            },
+            InterruptMessage::RemoveFromPlaylist(data) => {
+                let data_vec:Vec<&str> = data.split_whitespace().collect();
+
+                if data_vec.len() != 2 {
+                    continue;
+                }
+
+                remove_from_playlist(data_vec[0].to_string(), data_vec[1].parse()?)?;
+            },
+            InterruptMessage::QueueToPlaylist(playlist_name) => {
+                queue_to_playlist(playlist_name, &queue)?;
+            },
+            InterruptMessage::DisplayQueue => println!("{queue:?}")
+
         }
         if let Some(ref s) = &sink {
             if s.empty() && !queue.is_empty() {
@@ -262,10 +368,28 @@ pub fn execute(audiotx: Sender<InterruptMessage>) {
             command if command.starts_with("lp") => {
                 let playlist_name = command[3..].to_string();
                 audiotx.send(InterruptMessage::LoadPlaylist(playlist_name)).unwrap();
-            }
-            "h" => {
-                println!("walkman docs\nplay: p {{songname}}\nqueue: q {{songname}}\npz: pause\nr: resume\nstop: s\nexit: e\ndocs: h");
-            }
+            },
+            command if command.starts_with("crpl") => {
+                let playlist_name = command[5..].to_string();
+                audiotx.send(InterruptMessage::CreatePlaylist(playlist_name)).unwrap();
+            },
+            command if command.starts_with("dlpl") => {
+                let playlist_name = command[5..].to_string();
+                audiotx.send(InterruptMessage::DeletePlaylist(playlist_name)).unwrap();
+            },
+            command if command.starts_with("+pl") => {
+                let data = command[4..].to_string();
+                audiotx.send(InterruptMessage::AddToPlaylist(data)).unwrap();
+            },
+            command if command.starts_with("-pl") => {
+                let data = command[4..].to_string();
+                audiotx.send(InterruptMessage::RemoveFromPlaylist(data)).unwrap();
+            },
+            command if command.starts_with("q2p") => {
+                let playlist_name = command[4..].to_string();
+                audiotx.send(InterruptMessage::QueueToPlaylist(playlist_name)).unwrap();
+            },
+            "kyu" => audiotx.send(InterruptMessage::DisplayQueue).unwrap(),
             "e" => {
                 break;
             },
